d9bf20281fadf04b9a308db659b5907486700a7f
Delire Stéphane <116341760+stephane-delire@users.noreply.github.com>
2025-02-07 12:53:14 +0100

    [ADD] Dynamic styling

--- src/js/sql_panel.js
+++ src/js/sql_panel.js
@@ -1,8 +1,43 @@
 
-// Constants
+// --- Constants
+// btns
 const sqlBtnExecute = document.getElementById('sql_btn_execute');
 const sqlBtnClear = document.getElementById('sql_btn_trash');
+// textareas
+const sqlTextarea = document.getElementById('sql_textarea');
 
-// Event listeners
+// --- Event listeners
 sqlBtnExecute.addEventListener('click', executeSQL);
 sqlBtnClear.addEventListener('click', clearSQL);
+
+
+// --- Functions
+function executeSQL() {
+    let sql = sqlTextarea.value;
+    try {
+        alasql(sql);
+        successSQL();
+    } catch (error) {
+        console.error(error);
+        errorSQL();
+    }
+}
+
+function clearSQL() {
+    sqlTextarea.value = '';
+    resetSQL();
+    sqlTextarea.focus();
+}
+
+// styling the textarea
+function successSQL() {
+    // add class to the textarea
+    sqlTextarea.classList.add('sql-success');
+}
+function errorSQL() {
+    sqlTextarea.classList.add('sql-error');
+}
+function resetSQL() {
+    sqlTextarea.classList.remove('sql-success');
+    sqlTextarea.classList.remove('sql-error');
+}
