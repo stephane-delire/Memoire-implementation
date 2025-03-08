eea47c0078f36a8f672c7fd48b070e09375f925f
Delire Stéphane <116341760+stephane-delire@users.noreply.github.com>
2025-03-08 21:37:00 +0100

    Update service.sh

--- server/service.sh
+++ server/service.sh
@@ -1,4 +1,8 @@
+echo "Starting service"
 cd /home/azureuser/venv
+echo "Activating virtual environment"
 source bin/activate
+echo "Activating virtual environment done"
 cd /home/azureuser/Memoire-implementation/server
+echo "Starting server"
 python web.py
