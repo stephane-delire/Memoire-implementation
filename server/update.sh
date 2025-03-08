7a8d00f50702d9ef93796b61c95ca46bb6b5ba82
Delire Stéphane <116341760+stephane-delire@users.noreply.github.com>
2025-03-08 21:07:38 +0100

    [WIP] webserver

---
+++ server/update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Script de mise a jour automatique lors de l'appel d'un webhook
+# -------------------------------------------------------------
+
+# On se place dans le dossier du projet
+cd /home/azureuser/Memoire-implementation
+
+git pull origin main
+
+# On redémarre le sservice (serveur)
+sudo systemctl restart arewal-server.service
---
+++ server/web.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, send_file, send_from_directory, abort
+from waitress import serve
+import subprocess
+import os
+
+app = Flask(__name__)
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# ------------------------------------------------------------------------------
+# Index
+@app.route('/')
+def index():
+    with open('../main.html', 'r') as f:
+        return f.read()
+
+# ------------------------------------------------------------------------------
+# Static files
+@app.route('/<path:path>')
+def static_file(path):
+    file_path = os.path.join(PROJECT_ROOT, path)
+
+    # Vérifier si le fichier existe
+    if os.path.isfile(file_path):
+        return send_file(file_path)
+    else:
+        abort(404)
+
+# ------------------------------------------------------------------------------
+# Update route
+@app.route('/update', methods=['POST'])
+def update_server():
+    # Exécute le script de mise à jour
+    process = subprocess.run(["./update.sh"], capture_output=True, text=True)
+    return ''
+
+# ==============================================================================
+# Run the server
+if __name__ == '__main__':
+    serve(app, host='127.0.0.1', port=5050, threads=8, backlog=2048)
