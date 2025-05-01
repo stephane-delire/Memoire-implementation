38824613c7c7b7e6d14dd240dce45ea7cad2a1d5
Delire Stéphane <116341760+stephane-delire@users.noreply.github.com>
2025-05-01 17:00:26 +0200

    [WIP] 2eme version frontend

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ Fichier de serveur Flask pour servir l'application via web.
 -------------------------------------------------------------------------------
 """
 
-from flask import Flask
+from flask import Flask, request, send_file
 import waitress
 import os
 
@@ -12,10 +12,21 @@ from cqa.sources.certainty import certainty
 
 app = Flask(__name__)
 
+# =============================================================================
 # ----------------------------------------------------------------------- index
 @app.route('/', methods=['GET'])
 def index():
-    return 'Hello World!'
+    with open('src/html/main.html', 'r') as f:
+        html = f.read()
+    return html
+
+# ---------------------------------------------------------------------- static
+@app.route('/src/<path:path>')
+def send_src_file(path):
+    """
+    Fonction pour servir les fichiers statiques.
+    """
+    return send_file(os.path.join('src', path))
 
 # ------------------------------------------------------------------------- cqa
 @app.route('/cqa', methods=['POST'])
@@ -31,6 +42,6 @@ waitress.serve(
     app, 
     host='0.0.0.0', 
     port=8080,
-    threads=4,
+    threads=8,
     backlog=100,
     )
