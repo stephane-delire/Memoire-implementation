2e677a1e270e6ebd64b17e20cd3cc3e7ec658346
Delire Stéphane <116341760+stephane-delire@users.noreply.github.com>
2025-05-01 15:49:59 +0200

    moving files

--- cqa/main.py
+++ cqa/main.py
@@ -1,7 +1,17 @@
+"""
+-------------------------------------------------------------------------------
+Implémentation de l'algorithme certainty
+dans le cadre du Consistent Query Answering (CQA) with conjunctive queries
+and negated atoms.
+
+Delire Stéphane.
+-------------------------------------------------------------------------------
+"""
+
 from sources.certainty import certainty
 
-# with open("../good_one.cqa", "r") as f:
-with open("../03-false-certainty.cqa", "r") as f:
+# with open("examples/good_one.cqa", "r") as f:
+with open("examples/03-false-certainty.cqa", "r") as f:
     text = f.read()
 
 data, guarded, graph, cycle, certain = certainty(text, graph_png=False)
